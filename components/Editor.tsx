062f32b2618a2eda4d2e9aff2ea2bf8fdaf32a0f
Jordan-Tran01 <jordantran114@gmail.com>
2024-02-16 20:23:24 +1100

    making the editor page clearer

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -16,19 +16,26 @@ const Editor = ({ feed }) => {
 
     return (
 
-        <div className="w-full h-full">
+        <div className="w-screen h-screen relative">
             {isLoading && (<div>...loading</div>)}
+            <div className="border-black border-solid border-4">
+            <textarea className="p-8 text-xl outline-none placeholder:title" 
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)} 
+            />
+            </div>
+
+            <div className="border-black border-solid border-4 my-3">
             <textarea className="p-8 text-xl outline-none border-black" 
             value={content} 
             onChange={(e) => setContent(e.target.value)} 
             />
-            <textarea className="p-8 text-xl outline-none border-black" 
-            value={title} 
-            onChange={(e) => setTitle(e.target.value)} 
-            />
+            </div>
+            <div className="my-10">
             <button onClick={handleSave} className="ml-4 p-4 border-solid border-4 border-black">Save for later</button>
             <button onClick={handleSave} className="ml-4 p-4 border-solid border-4 border-black">Post!</button>
-            
+            </div>
+
         </div>
     )
 }
