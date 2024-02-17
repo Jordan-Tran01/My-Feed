2dd537935d065f1e6bc335d6884c5b4bcedc8b0b
Jordan-Tran01 <jordantran114@gmail.com>
2024-02-17 22:00:22 +1100

    created the browse page to display public feeds

--- app/(dashboard)/browse/page.tsx
+++ app/(dashboard)/browse/page.tsx
@@ -1,6 +1,37 @@
-const BrowsePage = () => {
+import { prisma } from "@/utils/db"
+import FeedCard from "@/components/FeedCard"
+import Link from "next/link"
+
+const getPublicFeeds = async () => { //Function to extract the feeds of the specific user
+    const feeds = await prisma.feedEntry.findMany({
+        where: {
+            public: true,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+
+    return feeds
+}
+
+
+const BrowsePage = async () => {
+    const feeds = await getPublicFeeds()
+
     return (
-        <div>Browse global feed page</div>
+        <div className="p-6 bg-gray-50"> 
+        <h1 className="p-6 font-semibold text-gray-700 text-3xl">Browse and Comment on peoples feeds!</h1>
+        <div className="grid grid-cols-3 gap-4 p-5">
+        {feeds.map((feed) => (
+          <div key={feed.id}>
+            <Link href={`/home/${feed.id}`}>
+              <FeedCard feed={feed} />
+            </Link>
+        </div>
+        ))}
+      </div>
+    </div>
     )
 }
 
--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -4,10 +4,6 @@ import { getEmail } from "@/utils/email"
 import NewFeedCard from "@/components/NewFeedCard"
 import FeedCard from "@/components/FeedCard"
 import Link from 'next/link'
-import { clerkClient } from '@clerk/nextjs';
-import { auth } from "@clerk/nextjs"
-
-import { currentUser } from '@clerk/nextjs'
 
 
 const getFeeds = async () => { //Function to extract the feeds of the specific user
@@ -27,15 +23,13 @@ const getFeeds = async () => { //Function to extract the feeds of the specific u
 const HomePage = async () => {
     const feeds = await getFeeds()
     const email = await getEmail()
-    //const userId = await auth()
-    //const user = await clerkClient.users.getUser(userId.userId);
     const user = await getUserFromClerkID()
     const username = user.username;
+
     return ( 
     <div className="p-6 bg-gray-50"> 
-        <div className="py-8">{username}'s drafts</div>
         <NewFeedCard />
-        <h1 className="p-6 font-semibold text-gray-700 text-3xl">Drafted Feeds</h1>
+        <h1 className="p-6 font-semibold text-gray-700 text-3xl">{username}'s Drafted Feeds</h1>
         <div className="grid grid-cols-3 gap-4 p-5">
         {feeds.map((feed) => (
           <div key={feed.id}>
